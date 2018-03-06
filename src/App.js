3e44c607bf67bc4a2f525535eb38472e3231d33b
brookerodenhizer <brooketaylorrodenhizer@gmail.com>
2018-03-05 23:17:01 -0500

    checkpoint 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+import { RoomList } from './components/RoomList.js';
+
+var config = {
+  apiKey: "AIzaSyAe2jTRgHYTrqM08Uq4R8CWei3GgrKpDCk",
+  authDomain: "bloc-chat-react-b36ac.firebaseapp.com",
+  databaseURL: "https://bloc-chat-react-b36ac.firebaseio.com",
+  projectId: "bloc-chat-react-b36ac",
+  storageBucket: "bloc-chat-react-b36ac.appspot.com",
+  messagingSenderId: "55903431811"
+};
+firebase.initializeApp(config);
 
 class App extends Component {
   render() {
