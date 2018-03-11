383c0f71521ef394bd8de51f2810f5336d041b07
brookerodenhizer <brooketaylorrodenhizer@gmail.com>
2018-03-11 15:28:08 -0400

    checkpoint 2 resubmit

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 export class RoomList extends Component {
   constructor(props) {
     super(props);
-      this.state = {rooms: []};
+      this.state = {
+        rooms: []
+      };
       this.roomsRef = this.props.firebase.database().ref('rooms');
   }
 
@@ -19,11 +21,11 @@ export class RoomList extends Component {
   render () {
     return (
         <div>
-          {
-            this.state.rooms.map( (room, index) =>
-              <li className="room" key={index}>{room.name}</li>
-            )
-          }
+        {this.state.rooms.map ((room, i) => (  //to loop over the room array to render its contents Q: why state and not props?
+        <li key={room.key}>{room.name}</li>
+            // {this.state.rooms.map ((room) => (  //to loop over the room array to render its contents
+              // <h1>{room.key}</h1>
+        ))}
         </div>
     );
   }
