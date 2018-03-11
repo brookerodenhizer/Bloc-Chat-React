c8aac557936edf39fddeab39d73b20ccf7f18b73
brookerodenhizer <brooketaylorrodenhizer@gmail.com>
2018-03-11 19:25:45 -0400

    create chat rooms

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -3,8 +3,20 @@ import React, { Component } from 'react';
 export class RoomList extends Component {
   constructor(props) {
     super(props);
-      this.state = { rooms: [] };
+      this.state = {newRoomName: "", rooms: []};
       this.roomsRef = this.props.firebase.database().ref('rooms');
+      this.handleChange = this.handleChange.bind(this);
+      this.createRoom = this.createRoom.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ newRoomName: e.target.value });
+  }
+
+  createRoom(e) {
+    e.preventDefault();
+    this.roomsRef.push({ newRoomName: this.state.newRoomName });
+    this.setState({ newRoomName: ""});
   }
 
   componentDidMount() {
@@ -15,12 +27,23 @@ export class RoomList extends Component {
     });
   }
 
+
   render() {
+    const newRoom = (
+      <form onSubmit={this.createRoom}>
+        <input type="text" value={this.state.newRoomName} placeholder="Create New Room" onChange={this.handleChange}/>
+        <input type="submit" value="Create" />
+      </form>
+    );
     const roomList = this.state.rooms.map((room) =>
-      <li key={room.key}>{room.name}</li>
-  );
+      <li key={room.key}>{room.newRoomName}</li>
+    );
+
     return (
-      <ul>{roomList}</ul>
+      <div>
+      <div>{newRoom}</div>
+        <ul>{roomList}</ul>
+      </div>
     );
   }
 }
